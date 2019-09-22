719f439fe5a8d7637aa53bcd874b002189cbd54c
Liam Daley <daleylp@hotmail.com>
2019-09-22 11:36:08 +1000

    Single Page Applications: Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
-
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const Dish = (props) => {
     const dish = props.dish
@@ -45,13 +45,23 @@ const DishDetail = (props) => {
     const dish = props.dish
     let comments = []
     if (dish) {
-       comments = props.dish.comments
+       comments = props.comments
     } else {
         return <div></div>
     }
 
     return (
         <div className="container">
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr />
+                </div>
+            </div>
             <div className="row">
                 <div className="col-12 col-md-5 m-1">
                     {dish && <Dish dish={dish} />}
